1e267103492aef2c878e9857bfc3a2f3e535ce3d
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-08-30 01:12:18 -0400

    pasword problem

--- javascrpit-conditionals/JS-conditionals-tasks/password-validator.js
+++ javascrpit-conditionals/JS-conditionals-tasks/password-validator.js
@@ -4,3 +4,35 @@ The password must be at least 8 characters long.
 It must contain at least one uppercase letter, one lowercase letter, and one number.
 If the password meets all criteria, display "Password is valid."
 If any criteria are not met, display "Password is invalid." */
+
+function checkPass(pass) {
+    let hasUppercase = /[A-Z]/.test(pass);
+    let hasLowercase = /[a-z]/.test(pass);
+    let hasNumber = /[0-9]/.test(pass);
+    let isInvalid = ((pass.length < 8) || (!hasUppercase) || (!hasLowercase) || (!hasNumber));
+    
+    if (pass.length < 8) {
+        console.log("Please add at least 8 characters");
+    }
+
+    if (!hasUppercase) {
+        console.log("Please include an uppercase letter");
+    }
+
+    if (!hasLowercase) {
+        console.log("Please include a lowercase letter");
+    }
+
+    if (!hasNumber) {
+        console.log("Please include a number");
+    }
+    
+    if (!isInvalid) {
+        console.log("The password meets all the criteria.");
+    }
+    else{
+        console.log("This is not a valid password!");
+    }
+}
+
+checkPass("abcd")
