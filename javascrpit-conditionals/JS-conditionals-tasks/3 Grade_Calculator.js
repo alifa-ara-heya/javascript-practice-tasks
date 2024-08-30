bc425d157f341ac47c10ed7f8d7287f974a20e17
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-08-29 20:48:04 -0400

    3rd problem done

--- javascrpit-conditionals/JS-conditionals-tasks/3 Grade_Calculator.js
+++ javascrpit-conditionals/JS-conditionals-tasks/3 Grade_Calculator.js
@@ -11,3 +11,24 @@ Create a simple JavaScript program that takes a student's score as input and ret
     F: 0-59
 
 ***/
+
+let score = -2;
+
+if (score >= 90 && score <= 100) {
+    console.log("Congratulations! You got A");
+}
+else if (score >= 80 && score < 90) {
+    console.log("Congratulations! You got B");
+}
+else if (score >= 70 && score < 80) {
+    console.log("Oops! You got C");
+}
+else if (score >= 60 && score < 70) {
+    console.log("Oops! You got D");
+}
+else if (score >= 0 && score < 60) {
+    console.log("Oh No! You Failed. Go to study");
+}
+else {
+    console.log("Invalid Score");
+}
