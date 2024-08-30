11c5d797085b79697d5e08cf6380a450dfbd9eb2
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-08-29 22:12:00 -0400

    6th problem done

--- javascrpit-conditionals/JS-conditionals-tasks/6 bus fare calculator.js
+++ javascrpit-conditionals/JS-conditionals-tasks/6 bus fare calculator.js
@@ -6,3 +6,22 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+
+let age = 70;
+let isStudent = true;
+let fare = 800;
+
+if (age < 10) {
+    console.log("free");
+}
+else if (isStudent) {
+    fare = 800 - 800 * 50 / 100;
+    console.log(`After 50% discount, your fare is ${fare} tk.`);
+}
+else if (age >= 60) {
+    fare = 800 - 800 * 15 /100;
+    console.log(`After 15% discount, your fare is ${fare} tk.`);
+}
+else{
+    console.log("800 tk");
+}
