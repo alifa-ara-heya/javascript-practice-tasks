61d10ba9f22742a34f0626b4d9665d9fcfb01278
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-08-30 01:47:01 -0400

    leap year problem

--- javascrpit-conditionals/JS-conditionals-tasks/leap-year-checker.js
+++ javascrpit-conditionals/JS-conditionals-tasks/leap-year-checker.js
@@ -1,3 +1,45 @@
 /* Write a program that takes a year as input and checks whether it is a leap year.
 A year is a leap year if it is divisible by 4 but not by 100, unless it is also divisible by 400.
 Display "Leap year" or "Not a leap year" accordingly. */
+
+function checkLeapYear(year) {
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            console.log("Leap Year");
+        }
+        else {
+            console.log("Not Leap year");
+        }
+    }
+    else {
+        if (year % 4 == 0) {
+            console.log("Leap Year");
+        }
+
+        else {
+            console.log("Not Leap year");
+        }
+    }
+
+}
+
+checkLeapYear(2000); //leap year
+checkLeapYear(1900); //Not Leap year
+checkLeapYear(2020); //Leap Year
+
+//another way
+function checkLeapYear2(year) {
+    if (year % 400 == 0) {
+        console.log("Leap Year");
+    } else if (year % 100 == 0) {
+        console.log("Not Leap Year");
+    } else if (year % 4 == 0) {
+        console.log("Leap Year");
+    } else {
+        console.log("Not Leap Year");
+    }
+}
+
+checkLeapYear2(2000); // Leap Year
+checkLeapYear2(1900); // Not Leap Year
+checkLeapYear2(2020); // Leap Year
